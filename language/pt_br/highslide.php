be997df042333cd5f04f18d05067a1b233a2f07f
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-11-07 21:43:45 -0200

    Traducao ADD

---
+++ language/pt_br/highslide.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*
+* @package phpBB Extension - Highslide
+* @license GNU General Public License, version 2 (GPL-2.0)
+* Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 0.0.2] (https://github.com/phpBBTraducoes)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOADINGTEXT'		=> 'Carregando...',
+	'LOADINGTITLE'		=> 'Clique para cancelar',
+	'FOCUSTITLE' 		=> 'Clique para trazer para a frente',
+	'FULLEXPANDTITLE'	=> 'Expandir para tamanho máximo (f)',
+	'PREVIOUSTEXT'		=> 'Anterior',
+	'CLOSETEXT'			=> 'Fechar',
+	'CLOSETITLE'		=> 'Fechar (esc)',
+	'RESIZETITLE'		=> 'Redimensionar',
+	'PLAYTEXT' 			=> 'Reproduzir',
+	'PLAYTITLE'			=> 'Reproduzir apresentação (barra de espaço)',
+	'PAUSETEXT'			=> 'Pausar',
+	'PAUSETITLE'		=> 'Pausar apresentação (barra de espaço)',
+	'PREVIOUSTITLE'		=> 'Anterior (seta direcional para esquerda)',
+	'NEXTTITLE'			=> 'Próxima (seta direcional para direita)',
+	'NUMBER'			=> 'Imagem %1 of %2',
+	'RESTORETITLE' 		=> 'Clique para fechar. Clique e arraste para mover. Use as setas direcionais para ir à anterior ou próxima.'
+));
---
+++ language/pt_br/info_acp_highslide.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* @package phpBB Extension - Highslide
+* @license GNU General Public License, version 2 (GPL-2.0)
+* Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 0.0.2] (https://github.com/phpBBTraducoes)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_HIGH_SLIDE'				=> 'Highslide',
+	'ACP_HIGH_SLIDE_CONFIG'			=> 'Configuração',
+	'ACP_HIGHSLIDE_EXPLAIN'			=> '',
+	'ACP_HIGH_SLIDE_TYPE'			=> 'Modo Mini Galeria',
+	'ACP_HIGH_SLIDE_TYPE_EXPLAIN'	=> 'Se ativado, as miniaturas de imagens clicáveis ​​adicionais serão exibidas para fácil navegação',
+	'UPDATE_CONFIG_SUCCESS'			=> 'Configuração alterada com sucesso'
+
+));
